8a0da03d8054e97c3170e53a9caa8028b67e484f
Andrew Andrew <andreinitescu13@yahoo.com>
2026-05-25 16:53:43 +0300

    init2

--- src/main.cpp
+++ src/main.cpp
@@ -10,25 +10,47 @@
 Adafruit_ST7735 tft = Adafruit_ST7735(10, 9, 8);
 hd44780_I2Cexp i2c = hd44780_I2Cexp(0x27, 2, 16);
 
+typedef struct {
+    byte instruction;
+    short arg1, arg2;
+} Instruction;
+
+unsigned int instructionCount = 0, currentMode = 0;
+
+Instruction instructions[128];
+
+// TODO: de inlocuit cod Arduino cu instructiuni AVR
+void i2c_display(int row, char *text) {
+    i2c.setCursor(0, row);
+    i2c.printf(text);
+}
+
+// PD5 - scroll through options (by increasing)
+ISR(PCINT21_vec) {
+
+}
+
+// PD6 - go backward through elements
+ISR(PCINT22_vec) {
+
+}
+
+// PD7 - go forward through elements
+ISR(PCINT23_vec) {
+
+}
+
 void setup() {
     Serial.begin(57600);
 
-    auto res = i2c.begin(16, 2);
-    Serial.printf("%d\n", res);
-    res = i2c.clear();
-
-    i2c.printf("1234567890abcdef");
-    Serial.printf("%d\n", res);
-
-    i2c.setCursor(0, 1);
-
-    i2c.printf("ghijklmnopqrstuv");
-
+    i2c.begin(16, 2);
 
     tft.initR(INITR_144GREENTAB);
     tft.fillScreen(ST77XX_RED);
 }
 
 void loop() {
-    
+    if (currentMode == 0) { // Set instruction count
+
+    }
 }
