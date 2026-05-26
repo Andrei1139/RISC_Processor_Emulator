6aba272c25b92ad264252a47a303224908cfbdd7
Andrei1139 <andreinitescu13@yahoo.com>
2026-05-26 23:51:56 +0300

    basic logic 2

--- src/main.cpp
+++ src/main.cpp
@@ -36,15 +36,15 @@ typedef struct {
     short arg0, arg1, arg2;
 } Instruction;
 
-volatile uint8_t instructionCount = 0, currentMode = 0;
+volatile byte instructionCount = 0, currentMode = 0;
 
 volatile Instruction instructions[128];
 short registers[16];
 short outRegister;
 
-volatile bool displayModified = true;
-volatile bool runProgram = true;
-volatile int currentElement = DEFAULT;
+volatile byte displayModified = 1;
+volatile byte runProgram = 0;
+volatile byte currentElement = DEFAULT;
 
 // TODO: ARDUINO -> AVR
 void i2cPrintText(int row, char *text) {
@@ -57,115 +57,107 @@ void i2cPrintInt(int row, int nr) {
     i2c.printf("%d", nr);
 }
 
-// PD5 - Scroll through options (by increasing)
-ISR(PCINT21_vec) {
-    if (currentMode == 0) {
-        instructionCount = (instructionCount + 1) % 128;
-    } else if (currentMode <= instructionCount) {
-        if (currentElement == CURR_INSTR) {
-            instructions[currentMode - 1].instruction = (instructions[currentMode - 1].instruction + 1) % AVAILABE_INSTRUCTIONS;
-            instructions[currentMode - 1].arg0 = 0;
-            instructions[currentMode - 1].arg1 = 0;
-            instructions[currentMode - 1].arg2 = 0;
-        } else if (currentElement == CURR_ARG0) {
-            switch (instructions[currentMode - 1].instruction) {
-                case ADD: case SUB: case INSERT_VAL: case INSERT_REG:
-                    instructions[currentMode - 1].arg0 = (instructions[currentMode - 1].arg0 + 1) % NUM_REGISTERS;
-                    break;
-                case JMP_IF_EQ:
-                    instructions[currentMode - 1].arg0 = (instructions[currentMode - 1].arg0 + 1) % instructionCount;
-                    break;
-            }
-        } else if (currentElement == CURR_ARG1) {
-            switch (instructions[currentMode - 1].instruction) {
-                case ADD: case SUB: case INSERT_REG: case JMP_IF_EQ:
-                    instructions[currentMode - 1].arg1 = (instructions[currentMode - 1].arg1 + 1) % NUM_REGISTERS;
-                    break;
-                case INSERT_VAL:
-                    instructions[currentMode - 1].arg1 = (instructions[currentMode - 1].arg1 == MAX_VALUE) ? MIN_VALUE :
-                                                          instructions[currentMode - 1].arg1 + 1;
-                    break;
-            }
-        } else if (currentElement == CURR_ARG2) {
-            switch (instructions[currentMode - 1].instruction) {
-                case ADD: case SUB: case JMP_IF_EQ:
-                    instructions[currentMode - 1].arg2 = (instructions[currentMode - 1].arg2 + 1) % NUM_REGISTERS;
-                    break;
+ISR(PCINT2_vect) {
+    // PD5 - Scroll through options (by increasing)
+    if (PIND & (1 << PD5)) {
+        if (currentMode == 0) {
+            instructionCount = (instructionCount + 1) % 128;
+        } else if (currentMode <= instructionCount) {
+            if (currentElement == CURR_INSTR) {
+                instructions[currentMode - 1].instruction = (instructions[currentMode - 1].instruction + 1) % AVAILABE_INSTRUCTIONS;
+                instructions[currentMode - 1].arg0 = 0;
+                instructions[currentMode - 1].arg1 = 0;
+                instructions[currentMode - 1].arg2 = 0;
+            } else if (currentElement == CURR_ARG0) {
+                switch (instructions[currentMode - 1].instruction) {
+                    case ADD: case SUB: case INSERT_VAL: case INSERT_REG:
+                        instructions[currentMode - 1].arg0 = (instructions[currentMode - 1].arg0 + 1) % NUM_REGISTERS;
+                        break;
+                    case JMP_IF_EQ:
+                        instructions[currentMode - 1].arg0 = (instructions[currentMode - 1].arg0 + 1) % instructionCount;
+                        break;
+                }
+            } else if (currentElement == CURR_ARG1) {
+                switch (instructions[currentMode - 1].instruction) {
+                    case ADD: case SUB: case INSERT_REG: case JMP_IF_EQ:
+                        instructions[currentMode - 1].arg1 = (instructions[currentMode - 1].arg1 + 1) % NUM_REGISTERS;
+                        break;
+                    case INSERT_VAL:
+                        instructions[currentMode - 1].arg1 = (instructions[currentMode - 1].arg1 == MAX_VALUE) ? MIN_VALUE :
+                                                            instructions[currentMode - 1].arg1 + 1;
+                        break;
+                }
+            } else if (currentElement == CURR_ARG2) {
+                switch (instructions[currentMode - 1].instruction) {
+                    case ADD: case SUB: case JMP_IF_EQ:
+                        instructions[currentMode - 1].arg2 = (instructions[currentMode - 1].arg2 + 1) % NUM_REGISTERS;
+                        break;
+                }
             }
         }
-    }
+    } else if (PIND & (1 << PD6)) { // PD6 - Go backwards through elements
+        // Can't go before instruction count
+        if (currentMode == 0) return;
+        
+        if (currentElement > 0) {
+            --currentElement;
+        } else { // Go back through instructions - find the current instruction element interacted with
+            --currentMode;
+            if (currentMode == 0) {
+                currentElement = DEFAULT;
+            } else {
+                switch (instructions[currentMode - 1].instruction) {
+                    case ADD: case SUB: case JMP_IF_EQ:
+                        currentElement = CURR_ARG2;
+                        break;
+                    case INSERT_VAL: case INSERT_REG:
+                        currentElement = CURR_ARG1;
+                        break;
+                    case DISPLAY:
+                        currentElement = CURR_ARG0;
+                        break;
+                    default:
+                        currentElement = CURR_INSTR;
+                }
+            }
+        }
+    } else if (PIND & (1 << PD7)) { // PD7 - go forward through elements
+        // Moving forward inside runnable program - run again
+        if (currentMode == instructionCount + 1) {
+            runProgram = true;
+            return;
+        }
 
-    displayModified = true;
-}
-
-// PD6 - Go backwards through elements
-ISR(PCINT22_vec) {
-    // Can't go before instruction count
-    if (currentMode == 0) return;
-    
-    if (currentElement > 0) {
-        --currentElement;
-    } else { // Go back through instructions - find the current instruction element interacted with
-        --currentMode;
-        if (currentMode == 0) {
-            currentElement = DEFAULT;
+        // No elements - simply go to the next mode
+        if (currentMode == 0 || instructions[currentMode + 1].instruction == PASS) {
+            ++currentMode;
         } else {
-            switch (instructions[currentMode - 1].instruction) {
+            switch (instructions[currentMode + 1].instruction) {
                 case ADD: case SUB: case JMP_IF_EQ:
-                    currentElement = CURR_ARG2;
+                    if (currentElement < 3) {
+                        ++currentElement;
+                    } else {
+                        ++currentMode;
+                        currentElement = 0;
+                    }
                     break;
                 case INSERT_VAL: case INSERT_REG:
-                    currentElement = CURR_ARG1;
+                    if (currentElement < 2) {
+                        ++currentElement;
+                    } else {
+                        ++currentMode;
+                        currentElement = 0;
+                    }
                     break;
                 case DISPLAY:
-                    currentElement = CURR_ARG0;
+                    if (currentElement < 1) {
+                        ++currentElement;
+                    } else {
+                        ++currentMode;
+                        currentElement = 0;
+                    }
                     break;
-                default:
-                    currentElement = CURR_INSTR;
             }
-        }
-    }
-
-    displayModified = true;
-}
-
-// PD7 - go forward through elements
-ISR(PCINT23_vec) {
-    // Moving forward inside runnable program - run again
-    if (currentMode == instructionCount + 1) {
-        runProgram = true;
-        return;
-    }
-
-    // No elements - simply go to the next mode
-    if (currentMode == 0 || instructions[currentMode + 1].instruction == PASS) {
-        ++currentMode;
-    } else {
-        switch (instructions[currentMode + 1].instruction) {
-            case ADD: case SUB: case JMP_IF_EQ:
-                if (currentElement < 3) {
-                    ++currentElement;
-                } else {
-                    ++currentMode;
-                    currentElement = 0;
-                }
-                break;
-            case INSERT_VAL: case INSERT_REG:
-                if (currentElement < 2) {
-                    ++currentElement;
-                } else {
-                    ++currentMode;
-                    currentElement = 0;
-                }
-                break;
-            case DISPLAY:
-                if (currentElement < 1) {
-                    ++currentElement;
-                } else {
-                    ++currentMode;
-                    currentElement = 0;
-                }
-                break;
         }
     }
 
@@ -206,8 +198,8 @@ void setup() {
 
     i2c.begin(16, 2);
 
-    tft.initR(INITR_144GREENTAB);
-    tft.fillScreen(ST77XX_RED);
+    // tft.initR(INITR_144GREENTAB);
+    // tft.fillScreen(ST77XX_RED);
 
     // Enable input pins
     DDRD &= ~(1 << PD5);
@@ -226,18 +218,18 @@ void setup() {
 }
 
 void loop() {
-    if (displayModified) {
+    if (displayModified == 1) {
         cli();
         if (currentMode == 0) { // Set instruction count
             i2cPrintText(0, "Instruction cnt:");
             i2cPrintInt(1, instructionCount);
         }
 
-        displayModified = false;
+        displayModified = 0;
         sei();
     }
 
-    if (runProgram) { // Run program
+    if (runProgram == 1) { // Run program
         cli();
         for (int i = 0; i < instructionCount; ++i) {
             runInstruction(instructions + i);
@@ -246,7 +238,7 @@ void loop() {
             _delay_ms(500);
         }
         
-        runProgram = false;
+        runProgram = 0;
         sei();
     }
 }
